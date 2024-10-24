f8f78fa363875b6bc2990dee126499c203d81037
Dev-14 <Dev-14@DESKTOP-ERVJ1QA>
2024-10-24 19:04:21 +0500

    commit24oct

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://api.yourproduction.com/api', 
+    socketUrl: 'wss://ws.yourproduction.com',
+    version: '1.0.0',
+    defaultLanguage: 'en'
+};
