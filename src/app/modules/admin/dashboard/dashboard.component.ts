447f5b9c52fdd0f85083e13888dfa4619a634b76
sheharyarabid <iamsheharyarabid@gmail.com>
2024-11-22 13:14:36 +0500

    dashboard component added

---
+++ src/app/modules/admin/dashboard/dashboard.component.ts
@@ -0,0 +1,43 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { UserService } from 'app/core/user/user.service';
+import { User } from 'app/core/user/user.types';
+import { Subject, takeUntil } from 'rxjs';
+
+@Component({
+    selector     : 'dashboard',
+    standalone   : true,
+    templateUrl  : './dashboard.component.html',
+    encapsulation: ViewEncapsulation.None,
+    imports      : [ CommonModule, MatIcon, MatMenuModule],
+})
+export class dashboardComponent implements OnInit, OnDestroy 
+{   
+    loading: boolean = true;
+    user: User | null = null;
+    private _unsubscribeAll: Subject<any> = new Subject<any>();
+    constructor(private _userService: UserService,  private _changeDetectorRef: ChangeDetectorRef)
+    { }
+
+    ngOnInit(): void {
+        this._userService.get().pipe(
+            takeUntil(this._unsubscribeAll)
+        ).subscribe((user: any) => {
+            this.loading=true;
+            this.user = user;
+            this.loading = false; 
+            this._changeDetectorRef.markForCheck();
+        }, () => {
+            this.loading = false; 
+        });
+        console.log(` USER ${this.user}`);
+    }
+    ngOnDestroy(): void {
+        // Unsubscribe from all subscriptions
+        this._unsubscribeAll.next(null);
+        this._unsubscribeAll.complete();
+    }
+
+}
---
+++ src/app/modules/admin/dashboard/dashboard.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { dashboardComponent } from './dashboard.component';
+
+export default [
+    {
+        path     : '',
+        component: dashboardComponent,
+    },
+] as Routes;
