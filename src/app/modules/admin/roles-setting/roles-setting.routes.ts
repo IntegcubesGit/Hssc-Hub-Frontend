fa64a9753db44645c1adad4030cbafc015602e86
Muhammad Taqi Haider <66321573+Taqi495@users.noreply.github.com>
2024-11-30 18:29:14 +0500

    Roles Component Created And Feilds Added

---
+++ src/app/modules/admin/roles-setting/roles-setting.routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from "@angular/router";
+import { ListComponent } from "./list/list.component";
+import { CreateComponent } from "./create/create.component";
+
+
+export default [
+    {
+        path: '',
+        redirectTo: 'list',
+        pathMatch: 'full',
+    },
+    {
+        path: 'list',
+        component: ListComponent,
+
+    },
+    {
+        path: 'create/:id',
+        component: CreateComponent,
+    },
+] as Routes;
